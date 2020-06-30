b6fbc666f579629a71a70d30e043643551a143ca
Sam Escobar <sescobar8714@gmail.com>
2020-06-30 12:22:45 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeInt = parseInt(time.slice(0,2))
+  if (timeInt < 12) {
+    return "Good Morning"
+  }
+  if (timeInt >= 12 && timeInt < 17) {
+    return "Good Afternoon"
+  }
+  if (timeInt >= 17){
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+// function displayMessage(greet) {  
+//   const greeting = greet
+//   const content = document.getElementById("greeting")
+//   content.innerHTML = greeting
+// }
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
